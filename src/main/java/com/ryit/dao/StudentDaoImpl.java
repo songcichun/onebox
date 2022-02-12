62f8b638223a389a8444af3628406e59eaa8db4c
ryitedu <3207093479@qq.com>
2022-02-12 11:51:46 +0800

    add entity dao

---
+++ src/main/java/com/ryit/dao/StudentDaoImpl.java
@@ -0,0 +1,14 @@
+package com.ryit.dao;
+
+/**
+ * Description:
+ *
+ * @author soncihun
+ * @date 2022/2/12 11:50
+ */
+public class StudentDaoImpl {
+    public void run(){
+        System.out.println("Hello Word");
+    }
+
+}
---
+++ src/main/java/com/ryit/entity/Student.java
@@ -0,0 +1,12 @@
+package com.ryit.entity;
+
+/**
+ * Description:
+ *
+ * @author soncihun
+ * @date 2022/2/12 11:50
+ */
+public class Student {
+
+
+}
